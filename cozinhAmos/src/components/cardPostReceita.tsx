ee736fa84bbeb7dcafd2e68f658a9b1738f3fda9
Cássio Ritse <cassioritse@.com>
2023-09-25 16:08:16 -0300

    style(components/cardPostReceita): estilizando componente de post Receite

--- cozinhAmos/src/components/cardPostReceita.tsx
+++ cozinhAmos/src/components/cardPostReceita.tsx
@@ -1,16 +1,52 @@
-import { Avatar, Card } from "react-native-paper";
+import { Avatar, Card, Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 
 export default function CardPostReceita() {
-    return (
-    <Card>
-        <Card.Title
-            title='Mac Cheese'
-            subtitle='Julia Amaral'
-            left={(props) => <Avatar.Image size={55} source={require('../assets/logo.png')} />}
+  return (
+    <View>
+      <View style={style.header}>
+        <View style={style.imgProfile}>
+          <Avatar.Image size={55} source={require("../assets/logo.png")} />
+        </View>
+        <View>
+          <Text style={style.nameRecipe}>Mac Chesse</Text>
+          <Text style={style.nameProfile}>Julia Sthal</Text>
+        </View>
+      </View>
+      <Card>
+        <Card.Cover
+          source={{ uri: "https://picsum.photos/700" }}
+          style={style.cardCover}
         />
-        <Card.Content>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-    </Card>
-    );
-};
+      </Card>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: -3,
+    zIndex: 1,
+  },
+
+  imgProfile: {
+    backgroundColor: "white",
+    borderRadius: 100,
+    padding: 5,
+  },
+  cardCover: {
+    marginTop: -22,
+  },
+  nameProfile: {
+    backgroundColor: "white",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  nameRecipe: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+});
--- cozinhAmos/src/screens/loginScreen.tsx
+++ cozinhAmos/src/screens/loginScreen.tsx
@@ -1,100 +1,105 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, Pressable, KeyboardAvoidingView, Platform } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Image,
+  Pressable,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export default function LoginScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    
-  };
+  const handleLogin = () => {};
 
   return (
-    <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} >
-    <View style={styles.container}>
-      <Image
-        source={require('../assets/logo.png')}
-        style={styles.logo}
-        />
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <Image source={require("../assets/logo.png")} style={styles.logo} />
         <Text style={styles.title}>CozinhAmos</Text>
-    </View>
-    <View style={styles.container}>
+      </View>
+      <View style={styles.container}>
         <TextInput
-        style={styles.input}
-        placeholder="Login"
-        onChangeText={(text) => setEmail(text)}
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        onChangeText={(text) => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <Pressable style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Entrar</Text>
-      </Pressable>
-      <Pressable style={styles.buttonRegistrar} onPress={handleLogin}>
-        <Text style={styles.buttonTextRegistrar}>Registrar</Text>
-      </Pressable>
-    </View>
+          style={styles.input}
+          placeholder="Login"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          secureTextEntry
+        />
+        <Pressable style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </Pressable>
+        <Pressable style={styles.buttonRegistrar} onPress={handleLogin}>
+          <Text style={styles.buttonTextRegistrar}>Registrar</Text>
+        </Pressable>
+      </View>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
-    color: 'lightgreen'
+    color: "lightgreen",
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 60,
     padding: 20,
-    borderBottomWidth: 2, 
-    borderBottomColor: 'lightgreen',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-
+    borderBottomWidth: 2,
+    borderBottomColor: "lightgreen",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
   logo: {
-    width: 250, 
-    height: 200, 
+    width: 250,
+    height: 200,
     marginBottom: 20,
   },
   button: {
-    backgroundColor: 'lightgreen', // Cor de fundo verde claro
+    backgroundColor: "lightgreen", // Cor de fundo verde claro
     borderRadius: 20, // Borda redonda
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '80%',
-    margin: 30, 
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    margin: 30,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
   },
   buttonRegistrar: {
-    backgroundColor: 'white', 
-    borderRadius: 20, 
-    borderColor: 'lightgreen',
+    backgroundColor: "white",
+    borderRadius: 20,
+    borderColor: "lightgreen",
     borderWidth: 3,
     padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '80%', 
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
   },
   buttonTextRegistrar: {
-    color: 'lightgreen',
+    color: "lightgreen",
     fontSize: 18,
   },
 });
