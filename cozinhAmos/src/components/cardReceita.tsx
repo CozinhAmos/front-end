9960efa2b694f9da2d40a0f89d2ba7c9243d57e1
Cássio Ritse <cassioritse@.com>
2023-09-25 21:46:48 -0300

    style(components/cardReceira): style do cardReceita da tela de pesquisa

--- cozinhAmos/src/components/cardReceita.tsx
+++ cozinhAmos/src/components/cardReceita.tsx
@@ -1,14 +1,46 @@
+import { View, Text, StyleSheet } from "react-native";
 import { Avatar, Card } from "react-native-paper";
 
-export default function CardReceita() {
-    return (
-    <Card>
-        <Card.Title
-            title='Mac Cheese'
+interface PropsCardReceita {
+  name: string;
+}
+
+export default function CardReceita(props: PropsCardReceita) {
+  return (
+    <View style={style.card}>
+      <View style={style.header}>
+        <Text style={style.nameRecipe}>{props.name}</Text>
+      </View>
+      <Card>
+        <Card.Cover
+          source={{ uri: "https://picsum.photos/700" }}
+          style={style.cardCover}
         />
-        <Card.Content>
-        </Card.Content>
-        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-    </Card>
-    );
-};
+      </Card>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  card: {
+    width: "50%",
+    flex: 1,
+    margin: 5,
+    borderRadius: 16,
+  },
+  header: {
+    alignSelf: "flex-start",
+    backgroundColor: "white",
+    borderBottomRightRadius: 16,
+    zIndex: 1,
+  },
+  cardCover: {
+    marginTop: -16,
+  },
+  nameRecipe: {
+    paddingHorizontal: 6,
+    paddingRight: 15,
+    fontSize: 12,
+    fontWeight: "400",
+  },
+});
